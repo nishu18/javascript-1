d7fb8819dff61a9ddbfecaa5d5bcc9b751b60cf1
nishu18 <99903771+nishu18@users.noreply.github.com>
2022-11-17 23:33:21 +0530

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,42 @@
+let a="nishant";
+let b=9;
+
+console.log((a+b));
+
+//question 2
+
+console.log(typeof (a+b))
+
+//question 3
+
+const a1={
+  name:"nishu",
+  rollno:56
+    
+}
+console.log(a1.name)
+//question 4
+
+a1['friend']="shubham"
+a1['name']="lavish"
+console.log (a1)
+
+//question 5
+
+const dict={
+  name:"whats is  yoy name",
+  roll :"your roll no.",
+  subject:"math is my favroute subject"
+
+}
+console.log(dict['roll'])
+
+//
+const item ={
+    name:"nishant",
+    rollno :89,
+    markes:86,
+    subject: "Math"
+    
+  }
+  console.log(item["markes"])
---
+++ js2.js
@@ -0,0 +1,55 @@
+//problem 1
+/*let age=prompt("what is your age")
+if(age>10&&age<20){
+  console.log("your age betewwwn 10&20")
+}else{
+  console.log("your age not between 10&20")
+}
+*/
+//problem 2
+
+// let age=prompt("what is your age")
+// switch(age){
+//   case '12':
+//     console.log("your age is 12")
+//     break
+//   case '13':
+//     console.log("your age is 13")
+//     break
+//   case '14':
+//     console.log("your age is 14")
+//     break
+//   case '15':
+//     console.log("your age is 15")
+//     break
+//   default:
+//     console.log("your age is not special")
+// }
+
+//problem 3
+
+// let num=prompt("what is your num")
+// num =Number.parseInt(num)
+
+// if(num%2==0 && num%3==0){
+//   console.log("your number is divisible 2 & 3")
+// }else{
+//   console.log("your number is not divisible 2 & 3")
+// }
+
+//problem 4
+// let num=prompt("what is your num")
+// num =Number.parseInt(num)
+
+// if(num%2==0 || num%3==0){
+//   console.log("your number is divisible 2 & 3")
+// }else{
+//   console.log("your number is not divisible 2 & 3")
+// }
+
+
+//problem 5
+
+let age =19
+let a=age>18 ? "you can drive" : "you cannot drive"
+console.log(a)
